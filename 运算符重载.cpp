9b294620bcfa9e51409c18de5ca025b22373d81d
MingZhang0 <1803461369@qq.com>
2022-12-13 15:40:42 +0800

    修改后

--- 运算符重载.cpp
+++ 运算符重载.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 class Complex{
+	private:
+		Complex *m_pc;
 	public:
 		double real;
 		double image;
@@ -8,6 +10,7 @@ class Complex{
 		Complex(){
 			real=0;
 			image=0;
+			this->m_pc=this;
 		}
 		Complex(double t_real,double t_image){
 			this->real=t_real;
@@ -25,25 +28,49 @@ class Complex{
 		friend Complex operator ++ (Complex & A,int){
 		    return Complex(A.real++,A.image);
 		}
-		friend Complex operator -- (Complex & A){
+		friend Complex operator ++(Complex&A){
+			return Complex(++A.real,A.image);
+		}
+		friend Complex operator -- (Complex & A,int){
 		    return Complex(A.real--,A.image);	
         }
-}
+        friend Complex operator -- (Complex& A){
+        	return Complex(--A.real,A.image);
+		}
+		Complex operator=(Complex B){
+			this->real=B.real;
+			this->image=B.image;
+			return *this;
+		}
+		Complex* operator->(){
+			if(m_pc!=NULL){
+				return m_pc;
+			}
+			static Complex s_complex(2.1,0.1);
+			return &s_complex;
+		}
+		
+};
 int main()
 {
 	Complex comp1;
 	comp1.real=1.0;
 	comp1.image=1.0;
-	comp1.display();
+	comp1.display();//(1,1)
 	Complex comp2(2.0,2.0);
-	comp2.display();
+	comp2.display();//(2,2)
 	comp1+comp2;
 	Complex comp3=comp1+comp2;
-	comp3.display();
+	comp3.display();//(3,3)
 	comp3++;
-	comp3.display();
+	comp3.display();//(4,3)
 	Complex comp4=comp2+comp3;
 	comp4--;
-	comp4.display();
+	comp4.display();//(5，5)
+	--comp1;
+	comp1.display();//(0,1)
+	++comp2;
+	comp2.display();//(3，2）
+	comp1->display();
 	return 0;
 }
